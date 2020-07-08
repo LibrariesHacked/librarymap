5af1565fcf315943631cb6f13f9bc0f4abf6c36f
Dave <DaveBathnes@users.noreply.github.com>
2020-07-09 00:23:57 +0100

    Updates

---
+++ precache-manifest.7244184f8fe93fe2f7fb5b9f32dfe543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410568e314f651f71c34021c5cbfc0cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c99e2cf891d7ed1f65a",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "7c99e2cf891d7ed1f65a",
+    "url": "/static/js/2.cc9dc657.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.cc9dc657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d1916896501d1e2b83",
+    "url": "/static/js/main.c4bc9b7b.chunk.js"
+  },
+  {
+    "revision": "236cc6903457cd323329",
+    "url": "/static/js/runtime-main.3e3ae9ab.js"
+  }
+]);
