0cec39c4baf167cd9f188dc27c03cda1647c699b
Dave <DaveBathnes@users.noreply.github.com>
2020-08-27 22:46:14 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c50d9c46d72da8e115a05a67617deb8.js"
+  "/precache-manifest.e14c444e54ee8eb6b74df681a36ec893.js"
 );
 
 self.addEventListener('message', (event) => {
