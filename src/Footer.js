e63ef1dddf9ee255b4020dce439bd89e8d95f017
Dave <DaveBathnes@users.noreply.github.com>
2020-09-16 18:23:43 +0100

    #22 Updating data page text

--- src/Footer.js
+++ src/Footer.js
@@ -49,11 +49,11 @@ function Footer () {
           <Typography variant='button'>
             <Link component={RouteLink} to='/data' title='Maintaining the data used on this site and licensing' className={classes.tapTarget}>Data</Link><br />
             <Link href='https://github.com/LibrariesHacked/librarymap' target='_blank' title='Project on GitHub' className={classes.tapTarget}>GitHub</Link><br />
-            <Link href='https://www.librarylab.co.uk' target='_blank' title='About the library lab projects and documentation for each' className={classes.tapTarget}>About Library lab</Link>
+            <Link href='https://www.librarylab.uk' target='_blank' title='About the library lab projects and documentation for each' className={classes.tapTarget}>About Library lab</Link>
           </Typography>
         </Grid>
         <Grid className={classes.footerRight} item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <Typography variant='body1' className={classes.footerText}>To the memory of Paul Rowe <FavoriteIcon color='primary' className={classes.loveIcon} /></Typography><br />
+          <Typography variant='body1' className={classes.footerText}>In memory of Paul Rowe <FavoriteIcon color='primary' className={classes.loveIcon} /></Typography><br />
           <Typography variant='button'>
             <Link href='https://www.mind.org.uk/donate/' title='Donate to Mind' className={classes.tapTarget} target='_blank'>Donate to Mind</Link>
           </Typography>
